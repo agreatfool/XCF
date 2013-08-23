675b47e6c366d8e2e759e86be32945367588565f
Jonathan Dai <nightghost5078@gmail.com>
2013-08-23 17:17:20 +0800

    Add socket base class.

---
+++ src/lib/socket/Socket.cpp
@@ -0,0 +1,48 @@
+#include "Socket.h"
+
+namespace XCF {
+
+    Socket::Socket(std::string host, uint16_t port):
+        socketHost(host), socketPort(port),
+        socketType(SocketType::TCP),
+        socketStatus(SocketStatus::Closed), logger(LogFactory::get()),
+        socketFd(INIT_SOCKET_FD) {}
+
+    Socket::Socket(std::string host, uint16_t port, uint16_t type):
+        socketHost(host), socketPort(port),
+        socketType(type),
+        socketStatus(SocketStatus::Closed), logger(LogFactory::get()),
+        socketFd(INIT_SOCKET_FD) {}
+
+    Socket::~Socket() {}
+
+    int32_t Socket::init() {
+        this->socketFd = socket(PF_INET, SOCK_STREAM, 0);
+
+        if (this->socketFd > 0) {
+            bzero(&this->socketAddr, sizeof(this->socketAddr));
+
+            this->socketAddr.sin_family = AF_INET;
+            this->socketAddr.sin_port = htons(this->socketPort);
+            this->socketAddr.sin_addr.s_addr = inet_addr(this->socketHost.c_str());
+
+            if (bind(this->socketFd, (struct sockaddr*) &this->socketAddr, sizeof(this->socketAddr)) < 0) {
+                this->logger->error("[Socket] socket binding failed!");
+                this->release();
+            }
+        } else {
+            this->logger->error("[Socket] socketFd initialization failed!");
+        }
+
+        return this->socketFd;
+    }
+
+    void Socket::release() {
+        if(this->socketFd > 0) {
+            close(this->socketFd);
+        }
+        this->socketFd = INVALID_SOCKET_FD;
+        this->socketStatus = SocketStatus::Closed;
+    }
+
+} /* namespace XCF */
---
+++ src/lib/socket/Socket.h
@@ -0,0 +1,57 @@
+#ifndef XCF_SOCKET_H_
+#define XCF_SOCKET_H_
+
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#include <ev.h>
+
+#include "../log/Log.h"
+
+namespace XCF {
+
+    #define INIT_SOCKET_FD     0
+    #define INVALID_SOCKET_FD -1
+
+    namespace SocketType {
+        enum SocketType {
+            TCP = IPPROTO_TCP,
+            UDP = IPPROTO_UDP
+        };
+    }
+
+    namespace SocketStatus {
+        enum SocketStatus {
+            Closed,
+            Opened,
+            Connected,
+            Error
+        };
+    }
+
+    class Socket {
+        public:
+            Socket(std::string host, uint16_t port);
+            Socket(std::string host, uint16_t port, uint16_t type);
+            virtual ~Socket();
+        protected:
+            // Inputed
+            std::string        socketHost;
+            uint16_t           socketPort;
+            // Type
+            uint16_t           socketType;
+            // Status
+            int16_t            socketStatus;
+            // Generated
+            Log*               logger;
+            int32_t            socketFd;
+            struct sockaddr_in socketAddr;
+            // Functions
+            int32_t init();
+            void release();
+    };
+
+} /* namespace XCF */
+
+#endif /* XCF_SOCKET_H_ */
