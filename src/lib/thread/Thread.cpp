0ec48f5ce462e7805c3916d99dc60f66cf67ed75
Jonathan Dai <nightghost5078@gmail.com>
2013-09-06 16:07:42 +0800

    Save Thread class changes.

--- src/lib/thread/Thread.cpp
+++ src/lib/thread/Thread.cpp
@@ -2,10 +2,146 @@
 
 namespace XCF {
 
-    Thread::Thread(): threadId(NULL) {
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    //-* Thread
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    void *threadStartFunc(void *argv) {
+        if (!argv) {
+            return 0;
+        }
+        Thread *thread = (Thread *)argv;
+
+        if (thread->init() < 0) {
+            return 0;
+        } else {
+            thread->run();
+            return 0;
+        }
+    }
+
+    Thread::Thread():
+        threadId(NULL), lock(NULL), cond(NULL), status(ThreadStatus::INITED), logger(LogFactory::get())
+    {
+        this->threadId = ThreadUtil::createThread(threadStartFunc, this);
+        this->lock = ThreadUtil::createLock();
+        this->cond = ThreadUtil::createCond();
     }
 
     Thread::~Thread() {
+        delete this->threadId;
+        ThreadUtil::destroyLock(this->lock);
+        ThreadUtil::destroyCond(this->cond);
+        delete this->logger;
     }
+
+    void Thread::stop() {
+        ThreadUtil::lock(this->lock);
+
+        this->status = ThreadStatus::STOPPED;
+        ThreadUtil::signalCond(this->cond);
+
+        ThreadUtil::unlock(this->lock);
+        ThreadUtil::joinThread(this->threadId);
+
+        this->logger->info("Thread stopped.");
+    };
+
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    //-* ThreadUtil
+    //-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+    pthread_t *ThreadUtil::createThread(ThreadStartFunc start, Thread *thread) {
+        pthread_t *threadId;
+        pthread_create(threadId, NULL, start, (void *)thread);
+        return threadId;
+    };
+
+    pthread_mutex_t *ThreadUtil::createLock() {
+        pthread_mutex_t *lock;
+        if (pthread_mutex_init(lock, NULL) < 0) {
+            LogFactory::error(Utility::stringFormat("[Thread] error in pthread_mutex_init: [%d] %s", errno, strerror(errno)));
+            return NULL;
+        } else {
+            return lock;
+        }
+    };
+
+    int32_t ThreadUtil::destroyLock(pthread_mutex_t *lock) {
+        if (pthread_mutex_destroy(lock) < 0) {
+            LogFactory::error(Utility::stringFormat("[Thread] error in pthread_mutex_destroy: [%d] %s", errno, strerror(errno)));
+            return INVALID_RESULT;
+        } else {
+            delete lock;
+            return VALID_RESULT;
+        }
+    };
+
+    int32_t ThreadUtil::lock(pthread_mutex_t *lock) {
+        if (pthread_mutex_lock(lock) < 0) {
+            LogFactory::error(Utility::stringFormat("[Thread] error in pthread_mutex_lock: [%d] %s", errno, strerror(errno)));
+            return INVALID_RESULT;
+        } else {
+            return VALID_RESULT;
+        }
+    };
+
+    int32_t ThreadUtil::unlock(pthread_mutex_t *lock) {
+        if (pthread_mutex_unlock(lock) < 0) {
+            LogFactory::error(Utility::stringFormat("[Thread] error in pthread_mutex_unlock: [%d] %s", errno, strerror(errno)));
+            return INVALID_RESULT;
+        } else {
+            return VALID_RESULT;
+        }
+    };
+
+    pthread_cond_t *ThreadUtil::createCond() {
+        pthread_cond_t *cond;
+        if (pthread_cond_init(cond, NULL) < 0) {
+            LogFactory::error(Utility::stringFormat("[Thread] error in pthread_cond_inits: [%d] %s", errno, strerror(errno)));
+            return NULL;
+        } else {
+            return cond;
+        }
+    };
+
+    int32_t ThreadUtil::destroyCond(pthread_cond_t *cond) {
+        if (pthread_cond_destroy(cond) < 0) {
+            LogFactory::error(Utility::stringFormat("[Thread] error in pthread_cond_destroy: [%d] %s", errno, strerror(errno)));
+            return INVALID_RESULT;
+        } else {
+            delete cond;
+            return VALID_RESULT;
+        }
+    };
+
+    int32_t ThreadUtil::waitCond(pthread_cond_t *cond, pthread_mutex_t *lock) {
+        if (pthread_cond_wait(cond, lock) < 0) {
+            LogFactory::error(Utility::stringFormat("[Thread] error in pthread_cond_wait: [%d] %s", errno, strerror(errno)));
+            return INVALID_RESULT;
+        } else {
+            return VALID_RESULT;
+        }
+    };
+
+    int32_t ThreadUtil::signalCond(pthread_cond_t *cond) {
+        if (pthread_cond_signal(cond) < 0) {
+            LogFactory::error(Utility::stringFormat("[Thread] error in pthread_cond_signal: [%d] %s", errno, strerror(errno)));
+            return INVALID_RESULT;
+        } else {
+            return VALID_RESULT;
+        }
+    };
+
+    int32_t ThreadUtil::joinThread(pthread_t *threadId) {
+        if (pthread_join(*threadId, NULL) < 0) {
+            LogFactory::error(Utility::stringFormat("[Thread] error in pthread_join: [%d] %s", errno, strerror(errno)));
+            return INVALID_RESULT;
+        } else {
+            return VALID_RESULT;
+        }
+    };
+
+    pthread_t ThreadUtil::getThreadId() {
+        return pthread_self();
+    };
 
 } /* namespace XCF */
--- src/lib/thread/Thread.h
+++ src/lib/thread/Thread.h
@@ -11,54 +11,115 @@
 
 namespace XCF {
 
-    typedef void *(ThreadStartFunc)(void *);
+    typedef void *(ThreadStartFunc)(void *); // argument (void *) actually is (Thread *)
+
+    namespace ThreadStatus {
+        enum ThreadStatus {
+            INITED,
+            BLOCKED,
+            RUNNING,
+            STOPPED
+        };
+    }
 
     class Thread {
         public:
             Thread();
             virtual ~Thread();
             /**
-             * Create a new thread, and return the thread id.
-             * The pointer of the threadId already initialized.
+             * Get thread id in "Thread".
              */
-            static inline pthread_t *createThread(pthread_t *threadId, ThreadStartFunc start) {
-                pthread_create(threadId, NULL, start, NULL);
-                return threadId;
+            inline pthread_t *getThreadId() {
+                return this->threadId;
             };
+            /**
+             * Initialize the thread.
+             */
+            virtual int32_t init() = 0;
+            /**
+             * Run the thread.
+             */
+            virtual void run() = 0;
+            /**
+             * Whether current can be blocked.
+             */
+            virtual bool canBeBlocked() = 0;
+            /**
+             * Wakeup the thread.
+             */
+            void wakeup();
+            /**
+             * Stop the thread.
+             */
+            void stop();
+            /**
+             * Check whether thread status is marked as BLOCKED or STOPPED.
+             * If true, process according logics.
+             */
+            void checkBlockedOrStopped();
+        protected:
+            pthread_t       *threadId;
+            pthread_mutex_t *lock;
+            pthread_cond_t  *cond;
+            uint16_t        status;
+            Log             *logger;
+    };
+
+    class ThreadUtil {
+        public:
+            ThreadUtil();
+            virtual ~ThreadUtil();
             /**
              * Create a new thread, and return the thread id.
              * A new pointer of the threadId would be created.
              */
-            static inline pthread_t *createThread(ThreadStartFunc start) {
-                pthread_t *threadId = (pthread_t *) malloc(sizeof(pthread_t));
-                return Thread::createThread(threadId, start);
-            };
+            static pthread_t *createThread(ThreadStartFunc start, Thread *thread);
             /**
              * Create a thread mutex, and return the mutex id.
              */
-            static inline pthread_mutex_t *createLock() {
-                pthread_mutex_t *lock = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
-                if (pthread_mutex_init(lock, NULL) < 0) {
-                    LogFactory::error(Utility::stringFormat("[Thread] error in init thread mutex: [%d] %s", errno, strerror(errno)));
-                    return NULL;
-                } else {
-                    return lock;
-                }
-            };
+            static pthread_mutex_t *createLock();
             /**
-             * Destory a thread mutex.
+             * Destroy a thread mutex.
              */
-            static inline int32_t destoryLock(pthread_mutex_t *lock) {
-                if (pthread_mutex_destroy(lock) < 0) {
-                    LogFactory::error(Utility::stringFormat("[Thread] error in destory thread mutex: [%d] %s", errno, strerror(errno)));
-                    return INVALID_RESULT;
-                } else {
-                    delete lock;
-                    return VALID_RESULT;
-                }
-            };
-        protected:
-            pthread_t *threadId;
+            static int32_t destroyLock(pthread_mutex_t *lock);
+            /**
+             * Lock a thread mutex.
+             */
+            static int32_t lock(pthread_mutex_t *lock);
+            /**
+             * Unlock a thread mutex.
+             */
+            static int32_t unlock(pthread_mutex_t *lock);
+            /**
+             * Create thread condition.
+             */
+            static pthread_cond_t *createCond();
+            /**
+             * Destroy thread condition.
+             */
+            static int32_t destroyCond(pthread_cond_t *cond);
+            /**
+             * Wait on a condition variable.
+             */
+            static int32_t waitCond(pthread_cond_t *cond, pthread_mutex_t *lock);
+            /**
+             * Unblock a thread waiting for a condition variable.
+             */
+            static int32_t signalCond(pthread_cond_t *cond);
+            /**
+             * Join a thread.
+             * Suspends the calling thread to wait for successful termination of the thread specified.
+             */
+            static int32_t joinThread(pthread_t *threadId);
+            /**
+             * Get the thread id via "pthread_self()".
+             * The return value is reference not a pointer!
+             */
+            static pthread_t getThreadId();
+    };
+
+    class ThreadPool {
+
     };
 
 } /* namespace XCF */
