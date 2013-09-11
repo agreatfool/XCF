317f23f1ff5f4718499829953d73a058fc77137b
Jonathan Dai <nightghost5078@gmail.com>
2013-09-11 16:12:19 +0800

    Use thread to handle libev event loop.

--- src/lib/event/Timer.cpp
+++ src/lib/event/Timer.cpp
@@ -1,18 +1,44 @@
 #include "Timer.h"
-#include "../event/Event.h"
-#include "../thread/Thread.h"
+#include "../utility/Utility.h"
 
 DEF_NS_XCF_BEGIN
 
-    EventPeriodic *Timer::instance = NULL;
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+//-* TimerThread
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+TimerThread::TimerThread(): Thread(), EventPeriodic() {}
 
-    Timer::~Timer() {}
+TimerThread::~TimerThread() {}
 
-    EventPeriodic *Timer::get() {
-        if (Timer::instance == NULL) {
-            Timer::instance = new EventPeriodic();
-        }
-        return Timer::instance;
+void TimerThread::run() {
+    /*
+     * Since this thread is used by libev event loop,
+     * the event loop would block the thread and check event,
+     * there is no real logic in this thread itself.
+     * So we overwrite the method "run()", to start the loop in it.
+     */
+    this->startLoop();
+}
+
+bool TimerThread::canBeBlocked() { return false; }
+
+void TimerThread::process() { /* no logic to process here */ }
+
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+//-* Timer
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+TimerThread *Timer::instance = NULL;
+ThreadLock Timer::lock = PTHREAD_MUTEX_INITIALIZER;
+
+Timer::~Timer() {}
+
+TimerThread *Timer::get() {
+    if (Utility::isNullPtr(Timer::instance)) {
+        ThreadUtil::lock(&Timer::lock);
+        Timer::instance = new TimerThread();
+        ThreadUtil::unlock(&Timer::lock);
     }
+    return Timer::instance;
+}
 
 DEF_NS_XCF_END
--- src/lib/event/Timer.h
+++ src/lib/event/Timer.h
@@ -2,22 +2,33 @@
 #define XCF_TIMER_H_
 
 #include "../../Common.h"
+#include "../event/Event.h"
+#include "../thread/Thread.h"
 
 DEF_NS_XCF_BEGIN
 
-    class EventPeriodic;
+class TimerThread: public Thread, public EventPeriodic {
+    public:
+        TimerThread();
+        virtual ~TimerThread();
+        virtual void run();
+    protected:
+       virtual bool canBeBlocked();
+       virtual void process();
+};
 
-    class Timer {
-        public:
-            virtual ~Timer();
-            static EventPeriodic *get();
-        protected:
-            Timer();
-            static EventPeriodic *instance;
-            // Stop the compiler generating methods of copy the object
-            Timer(Timer const& copy);            // Not Implemented
-            Timer& operator=(Timer const& copy); // Not Implemented
-    };
+class Timer {
+    public:
+        virtual ~Timer();
+        static TimerThread *get();
+    protected:
+        Timer();
+        static TimerThread *instance;
+        static ThreadLock  lock;
+        // Stop the compiler generating methods of copy the object
+        Timer(Timer const& copy);            // Not Implemented
+        Timer& operator=(Timer const& copy); // Not Implemented
+};
 
 DEF_NS_XCF_END
 
